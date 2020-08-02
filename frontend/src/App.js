2f37be2559d86582000fc98403320d88fcfc80ef
Nadia Aymbinderow <nadiaaymbin@gmail.com>
2020-08-02 19:16:26 +0300

    added selectable row option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import ActiveCallsTable from './features/components/active.calls.table.component';
-import MoreInfoDialog from './features/components/dialog.component';
 import { urlencoded } from 'body-parser';
 import { responsiveFontSizes } from '@material-ui/core';
+import MoreCallInfo from './features/components/more.call.info.component';
 
 function App() {
   const [calls, setCalls] = useState([]);
+  const [selectedRow, setSelectedRow] = useState(null);
 
   const callServer = (url) => {
     return fetch(url)
@@ -15,13 +16,14 @@ function App() {
 
   useEffect(() => {
     callServer('http://localhost:9000/api/calls')
-      .then(setCalls)  
+      .then(setCalls)
   }, []);
 
   return (
     <div className="App">
-      <ActiveCallsTable rows={calls}/>
-      <MoreInfoDialog />
+      <ActiveCallsTable rows={calls} onSelected={() => setSelectedRow(!selectedRow)} />
+      <button onClick={() => setSelectedRow(null)}>less info</button>
+      {selectedRow && <MoreCallInfo row={selectedRow} />}
     </div>
   );
 }
